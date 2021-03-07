b84a8e1839a79649ac4782ab5362eb594fc29644
Carcraftz <40304589+Carcraftz@users.noreply.github.com>
2021-03-06 22:40:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-# Siddharth Lohani
+# Siddharth Lohani -> Forked by @carcraftz
 # 2/22/2021
 
-# selenium imports
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 # schedule imports
 import schedule
 import time
 from datetime import date
+import requests
 
 # settings import
 import settings
@@ -17,14 +14,7 @@ import settings
 # twitter api wrapper import
 import tweepy
 
-# set up chrome
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--headless')
 
-#set up selenium
-driver = webdriver.Chrome(options=chrome_options)
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(settings.API_KEY, settings.API_SECRET)
@@ -34,61 +24,49 @@ auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 def fetchDataAndTweet():
+		countr = requests.get("https://projects.nj.com/coronavirus-tracker/data/counts.json")
+		countjson = countr.json()[-1]
+		totalCases = countjson["Total state cases"]
+		newCases = countjson["New cases in state"]
+		deathr = requests.get("https://projects.nj.com/coronavirus-tracker/data/deaths.json")
+		deathjson = deathr.json()[-1]
+		totalDeaths = deathjson["STATE TOTAL-MUST MATCH D"]
+		newDeaths = deathjson["New deaths"]
+		testsr = requests.get("https://projects.nj.com/coronavirus-tracker/data/tests.json")
+		testsjson = testsr.json()[-1]
+		totalTests = testsjson["Total Tests"]
+		hospitalr = requests.get("https://covidtracking.com/page-data/data/page-data.json")
 
-    print("function called")
-
-    today = date.today()
-
-    d = today.strftime("%B %d, %Y")
-
-    # fetch the link with covid cases
-    driver.get("https://projects.nj.com/coronavirus-tracker/")
-
-    time.sleep(20)
-
-    newCases = driver.find_element(By.XPATH, '//*[@id="day-cases-disp"]/div[2]').text
-
-    totalCases = driver.find_element(By.XPATH, '//*[@id="cum-cases-disp"]/div[2]').text
-
-    newDeaths = driver.find_element(By.XPATH, '//*[@id="day-deaths-disp"]/div[2]').text
-
-    totalDeaths = driver.find_element(By.XPATH, '//*[@id="cum-deaths-disp"]/div[2]').text
-
-    newTests = driver.find_element(By.XPATH, '//*[@id="day-tests-disp"]/div[2]').text
-
-    totalTests = driver.find_element(By.XPATH, '//*[@id="cum-tests-disp"]/div[2]').text
-
-    todaysPositivity = driver.find_element(By.XPATH, '//*[@id="day-pos-disp"]/div[2]').text
-
-    overallPositivity = driver.find_element(By.XPATH, '//*[@id="cum-pos-disp"]/div[2]').text
-
-
-    firstLine = "#COVID updates for " + d + ": \n \n"
-
-    newCasesLine = "new cases: " + newCases + "\n"
-
-    totalCasesLine = "total cases: " + totalCases + "\n \n"
-    
-    newDeathsLine = "new deaths: " + newDeaths + "\n"
-
-    totalDeathsLine = "total deaths: " + totalDeaths + "\n \n"
-
-    newTestsLine = "new tests: " + newTests + "\n" 
-
-    totalTestsLine = "total tests: " + totalTests + "\n \n"
-
-    todaysPositivityLine = "today's positivity: " + todaysPositivity
-
-    overallPositivityLine = "overall positivity: " + overallPositivity + "\n"
-
-    message = firstLine + newCasesLine + totalCasesLine + newDeathsLine + totalDeathsLine + newTestsLine +  totalTestsLine + overallPositivityLine
-    
-    print(message)
+		hospitaljson = hospitalr.json()["result"]["data"]["allCovidState"]
+		totalHospitalized = ""
+		newHospitalized = ""
+		for statedata in hospitaljson["nodes"]:
+			if(statedata["state"] =='NJ'):
+				totalHospitalized = str(statedata["hospitalizedCumulative"])
+				newHospitalized = str(statedata["hospitalizedCurrently"])
+		#TODO: figure out how to get new tests (prob need to access site on a weekday)
+		newTests = "0"
+		todaysPositivity = "0"
+		overallPositivity = str(round(int(totalCases)/int(totalTests)*100,3))+"%"
+		today = date.today()
+		d = today.strftime("%B %d, %Y")
+		
+		firstLine = "#COVID updates for " + d + ": \n \n"
+		newCasesLine = "new cases: " + newCases + "\n"
+		totalCasesLine = "total cases: " + totalCases + "\n \n"
+		newDeathsLine = "new deaths: " + newDeaths + "\n"
+		totalDeathsLine = "total deaths: " + totalDeaths + "\n \n"
+		newTestsLine = "new tests: " + newTests + "\n" 
+		totalTestsLine = "total tests: " + totalTests + "\n \n"
+		totalHospitalizedLine = "total hospitalized: " + totalHospitalized + "\n" 
+		newHospitalizedLine = "new hospitalized: " + newHospitalized + "\n\n" 
+		todaysPositivityLine = "today's positivity: " + todaysPositivity
+		overallPositivityLine = "overall positivity: " + overallPositivity + "\n"
+		message = firstLine + newCasesLine + totalCasesLine + newDeathsLine + totalDeathsLine + newTestsLine + totalTestsLine + totalHospitalizedLine + newHospitalizedLine +  overallPositivityLine
+		print(message)
 
     # tweet the update
-    api.update_status(message)
-
-    print("TWEETED")
+		api.update_status(message)
 
 fetchDataAndTweet()
 
